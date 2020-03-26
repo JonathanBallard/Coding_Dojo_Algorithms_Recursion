627be83c42e73bc43b7b0d6c9bcb5db4795f7a91
Jonathan Ballard <asgardschosen@gmail.com>
2020-03-26 10:59:16 -0700

    todo 1

---
+++ Todo1.js
@@ -0,0 +1,55 @@
+
+// Recursion ToDo1
+
+
+
+// Recursive Sigma
+function recursiveSigma(num){
+    if(num == 0){
+        return 0;
+    }
+    if(num > 0){
+        return num + recursiveSigma(num-1);
+    }
+}
+
+// console.log(recursiveSigma(5));
+
+
+
+function recursiveFactorial(num){
+    if(num <= 0){
+        return 1;
+    }
+    if(num > 0){
+        return num * recursiveFactorial(num-1);
+    }
+}
+
+
+// console.log(recursiveFactorial(5));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
